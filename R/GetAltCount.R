906102c3f7ec823219db3d0e169a7cac377d6405
raziafrooz <raziafro0z@gmail.com>
2023-05-19 12:13:50 -0400

    Add GitHub links to DESCRIPTION

--- R/GetAltCount.R
+++ R/GetAltCount.R
@@ -1,4 +1,17 @@
-#' @export
+
+#' Title
+#'
+#' @description This function will generate the alternative base counts for each loci. The input is the custom file format (.zst file) from
+#' the Recount3. This file includes read counts for the 4 bases. Therefore, in this function, we will find the base that has read counts
+#' mapped to it.
+#' @param alt_path The path that leads to the Recount3 alt.zst file
+#' @param sample_id_rep The individual sample ID to be genotyped
+#' @param filtered_snps_gr The biallelic SNP loci
+#' @param temp_folder Temporary folder to save unwanted files
+#'
+#'
+#' @return
+
 get_alt_count <- function(alt_path, sample_id_rep, filtered_snps_gr, temp_folder) {
   #Load in alt file to construct `alt_count`:
   #1. Decompress .zst via zstdcat in command line, save output as *.alt.temp.csv
@@ -22,16 +35,16 @@ get_alt_count <- function(alt_path, sample_id_rep, filtered_snps_gr, temp_folder
   #`alt_count` is 0-based, so we add 1 to positions.
   alt_count$pos <- alt_count$pos + 1
   #use chromosome names that are used in recount3.
-  chr_mapping <- fread(recount3_chr_mapping, header = FALSE)
+  chr_mapping <- function(...) data.table::fread(recount3_chr_mapping, header = FALSE)
   alt_count$chr <- chr_mapping$V2[match(alt_count$chr, chr_mapping$V1)]
-  alt_count_gr <- GRanges(seqnames = alt_count$chr,
+  alt_count_gr <- function(...) GenomicRanges::GRanges(seqnames = alt_count$chr,
                           ranges = IRanges(alt_count$pos,
                                            alt_count$pos))
-  ov <- findOverlaps(alt_count_gr, filtered_snps_gr)
+  ov <- function(...) GenomicRanges::findOverlaps(alt_count_gr, filtered_snps_gr)
   #Subset `alt_count_gr`, `alt_count` to the positions from SNPs, and compute
   #its own unique key.
-  alt_count_gr <- alt_count_gr[queryHits(ov)]
-  alt_count <- alt_count[queryHits(ov)]
+  alt_count_gr <- alt_count_gr[GenomicRanges::queryHits(ov)]
+  alt_count <- alt_count[GenomicRanges::queryHits(ov)]
   alt_key <- paste(as.character(seqnames(alt_count_gr)), start(alt_count_gr),
                    filtered_snps_gr$ref_seq[subjectHits(ov)], alt_count$alt, sep = "_")
   #Even though `alt_key` is in the same positions as `filtered_snps_key`, many of the
