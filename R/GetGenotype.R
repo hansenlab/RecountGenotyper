c6118e2130e45b401db9fe25522b2f3cd73cf990
raziafrooz <raziafro0z@gmail.com>
2023-05-22 12:02:52 -0400

    Deleted files that were not needed. Also updated GetGenotype function to predict genotype both for within and out of lattice

--- R/GetGenotype.R
+++ R/GetGenotype.R
@@ -1,62 +1,61 @@
 #' Title
 #'
 #'
-#' @param model_path s
-#' @param model_lattice_path s
-#' @param modelPrior s
-#' @param M s
-#' @param S s
+#' @param M M values calculated from GetMandS function
+#' @param S S value calculated from GetMandS fucntion
+#' @param model_MS Genotyping model
+#' @param modelLattice A lookup table generated form the genotyping model for faster performance
+#' @import caret
 #' @export
-GetGenotype<-function(model_path, M, S) {
+GetGenotype<- function(model_MS, modelLattice, prior=c(0.93752717, 0.03951271, 0.02239503) , M, S) {
   lattice_max <- 8.5
-  #Load in model information.
-  if(all(is.na(modelPrior)) == T) {
-    modelPrior = c(.94, .04, .02)
-  }
   #Figure out which values of predictor S can be used for lattice lookup table.
   withinLattice_idx <- which(S <= lattice_max)
   outsideLattice_idx <- which(S > lattice_max)
-  cat("Proportion of S values that can be inferred using lattice: ", length(withinLattice_idx)/length(S), "\n")
-  cat("Number of S values to infer with model later: ", length(outsideLattice_idx), "\n")
-  #Compute likelihood * modelPrior for each genotype.
-  likelihood_times_modelPrior <- sapply(1:3, function(k){
+
+  #Compute likelihood * prior for each genotype.
+  likelihood_times_prior <- sapply(1:3, function(k){
     #Inference on mu_M and sd_M
-    mu_M <- rep(NA, length(withinLattice_idx))
-    sd_M <- rep(NA, length(withinLattice_idx))
-    if(k == 1) {
-      #modelLattice is a data.table with key set to its S values,
+    mu_M <- rep(NA, length(S))
+    sd_M <- rep(NA, length(S))
+    if(k == 1){
+      #model_MS_lattice is a data.table with key set to its S values,
       #so we use the key to get lattice values.
-      mu_M <- modelLattice[J(S[withinLattice_idx])]$mu_M_prediction_1
-      sd_M <- modelLattice[J(S[withinLattice_idx])]$sd_M_prediction_1
-    }else if(k == 2) {
-      mu_M <- modelLattice[J(S[withinLattice_idx])]$mu_M_prediction_2
-      sd_M <- modelLattice[J(S[withinLattice_idx])]$sd_M_prediction_2
-    }else if(k == 3) {
-      mu_M <- modelLattice[J(S[withinLattice_idx])]$mu_M_prediction_3
-      sd_M <- modelLattice[J(S[withinLattice_idx])]$sd_M_prediction_3
+      mu_M[withinLattice_idx] <- model_MS_lattice[J(S[withinLattice_idx])]$mu_M_prediction_1
+      sd_M[withinLattice_idx] <- model_MS_lattice[J(S[withinLattice_idx])]$sd_M_prediction_1
+      #then, for SNPs out of the lattice, we use the model predictions.
+      mu_M[outsideLattice_idx] <- caret::predict(model_MS[[1]][[1]], data.frame(mu_S = unlist(S[outsideLattice_idx])))
+      sd_M[outsideLattice_idx] <- caret::predict(model_MS[[1]][[2]], data.frame(mu_S = unlist(S[outsideLattice_idx])))
+    } else if(k == 2) {
+      mu_M[withinLattice_idx] <- model_MS_lattice[J(S[withinLattice_idx])]$mu_M_prediction_2
+      sd_M[withinLattice_idx] <- model_MS_lattice[J(S[withinLattice_idx])]$sd_M_prediction_2
+      mu_M[outsideLattice_idx] <- caret::predict(model_MS[[2]][[1]], data.frame(mu_S = unlist(S[outsideLattice_idx])))
+      sd_M[outsideLattice_idx] <- caret::predict(model_MS[[2]][[2]], data.frame(mu_S = unlist(S[outsideLattice_idx])))
+    } else if(k == 3) {
+      mu_M[withinLattice_idx] <- model_MS_lattice[J(S[withinLattice_idx])]$mu_M_prediction_3
+      sd_M[withinLattice_idx] <- model_MS_lattice[J(S[withinLattice_idx])]$sd_M_prediction_3
+      mu_M[outsideLattice_idx] <- caret::predict(model_MS[[3]][[1]], data.frame(mu_S = unlist(S[outsideLattice_idx])))
+      sd_M[outsideLattice_idx] <- caret::predict(model_MS[[3]][[2]], data.frame(mu_S = unlist(S[outsideLattice_idx])))
     }
-    sd_M[sd_M <= 0] = 0 #if we predict (extrapolate) any variance to be <= 0, set it to 0.
-    return(modelPrior[k] * dnorm(x = M[withinLattice_idx], mean = mu_M, sd = sd_M, log = FALSE))
+    sd_M[sd_M <= 0] <- 0 #if we predict (extrapolate) any variance to be <= 0, set it to 0.
+    return(prior[k] * dnorm(x = M, mean = mu_M, sd = sd_M, log = FALSE))
   })
 
 
   posterior <- sapply(1:3, function(k){
-    if(is.null(dim(likelihood_times_modelPrior))) { #if we only have one SNP that needs to be genotyped
-      return(likelihood_times_modelPrior[k] / sum(likelihood_times_modelPrior))
+    if(is.null(dim(likelihood_times_prior))) { #edge case: if we only have one SNP that needs to be genotyped
+      return(likelihood_times_prior[k] / sum(likelihood_times_prior))
     }else { #else, we have many SNPs in a dataframe
-      return(likelihood_times_modelPrior[, k] / rowSums(likelihood_times_modelPrior))
+      return(likelihood_times_prior[, k] / rowSums(likelihood_times_prior))
     }
   })
 
   #Pick genotype with the max posterior genotype as our prediction.
-  if(is.null(dim(posterior))) { #if we only have one SNP that needs to be genotyped
+  if(is.null(dim(posterior))) { #edge case: if we only have one SNP that needs to be genotyped
     predicted_genotype <- which(posterior == max(posterior))
   }else { #else, we have many SNPs in a dataframe
     predicted_genotype <- apply(posterior, MARGIN = 1, FUN = function(x) which(x == max(x)))
   }
 
-
-  return(list(predicted_genotype = predicted_genotype,
-              withinLattice_idx = withinLattice_idx,
-              outsideLattice_idx = outsideLattice_idx))
+  return(predicted_genotype)
 }
