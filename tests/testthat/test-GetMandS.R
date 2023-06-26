bd9f6bf5f95f180f2dd2d8bd4bf0f265188e5045
raziafrooz <raziafro0z@gmail.com>
2023-06-26 11:09:11 -0400

    add extdata

---
+++ tests/testthat/test-GetMandS.R
@@ -0,0 +1,10 @@
+
+
+base_path<-system.file("extdata", package="RecountGenotyper")
+alt=paste0(base_path,"test.csv.zst")
+bw=paste0(base_path,"test.bw")
+
+
+test_that("M and S can be calculated", {
+  expect_equal(c(snps_path, bigWig_path=bw, coverage_cutoff=4,alt_path=alt, sample_id_rep="test", temp_folder="~"),)
+})
